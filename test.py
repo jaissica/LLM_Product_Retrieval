130c335c73bc8ae04160806c75e1d376c6c89340
Jaissica <jaissica.hora@gmail.com>
2024-12-13 21:40:46 -0500

    excluded api key

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import json
 from app import prompt_llm
 import os
 
-openai.api_key = "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"
+openai.api_key = ""
 
 response_links_model_3 = prompt_llm("Get me links of headphones", model_name="3.5")
 assert any("sony" in link.lower() for link in response_links_model_3), "No Sony link found in the results"
